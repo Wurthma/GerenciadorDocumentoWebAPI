143f775c4ef53b9f9fb2935670d649d2af4aae5f
Wurthmann <ge_h_w@hotmail.com>
2019-01-27 14:10:56 -0200

    Add project files.

---
+++ GerenciadorDocumentos/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace GerenciadorDocumentos.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ GerenciadorDocumentos/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GerenciadorDocumentos.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
