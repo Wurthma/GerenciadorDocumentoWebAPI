b3b4eeefef65113ee29df1faebdf79f24f2e404d
Wurthmann <ge_h_w@hotmail.com>
2019-02-05 20:38:42 -0200

    Remoção da pasta GerenciadorDocumentos.

--- GerenciadorDocumentos/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace GerenciadorDocumentos.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- GerenciadorDocumentos/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace GerenciadorDocumentos.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
